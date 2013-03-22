7cf6436b1f9a665f93c9c803075f3ec9dc2a921a
Bayo Erinle <berinle@gmail.com>
2013-03-22 13:07:31 -0400

    download and extract jmeter

--- manifests/main.pp
+++ manifests/main.pp
@@ -1,6 +1,7 @@
 include java
 include apt
 include stdlib
+
 package { ["vim",
 	           "curl",
 	           "git-core",
@@ -9,22 +10,14 @@ package { ["vim",
 	  ensure => present
 	  }
 
-#exec {
-#		"download_jmeter":
-#		command => "wget http://www.bizdirusa.#com/mirrors/apache//jmeter/binaries/apache-jmeter-2.9.tgz -O /usr/local/apache-jmeter-2.9.tgz",
-#		creates => "/vagrant/jmeter-2.9",		
-#		timeout => 0,
-#		cwd => "/vagrant",
-#		user => "vagrant",
-#		path => "/usr/bin/:/bin",
-#		logoutput => true,		
-#		require => Package[ "wget" ]
-#	}
-#
-#exec {
-#	"extract_jmeter":
-#	command => "tar -zxf apache-jmeter-2.9.tgz",
-#	cwd => "/usr/local",	
-#	unless => "test -d apache-jmeter-2.9",
-#    require => Exec["download_jmeter"]
-#}
+exec {
+		"download_jmeter":
+		command => "curl -L http://www.bizdirusa.com/mirrors/apache/jmeter/binaries/apache-jmeter-2.9.tgz | tar zx",
+		creates => "/vagrant/apache-jmeter-2.9",		
+		timeout => 0,
+		cwd => "/vagrant",
+		user => "vagrant",
+		path => "/usr/bin/:/bin",
+		#logoutput => true,		
+		require => Package[ "curl" ]
+	}
