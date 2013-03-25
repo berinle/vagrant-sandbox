81221e1fd2102255b6770b79f0846ecec854a988
Bayo Erinle <berinle@gmail.com>
2013-03-25 15:36:02 -0400

    cleaned up script and dependencies

--- manifests/main.pp
+++ manifests/main.pp
@@ -1,21 +1,20 @@
 include java
-include apt
-include stdlib
+#include apt
+#include stdlib
 
 package { ["vim",
 	           "curl",
 	           "git-core",
-	           "zsh",
-	           "wget"]:
+	           "zsh"]:
 	  ensure => present
 	  }
 
 exec {
 		"download_jmeter":
 		command => "curl -L http://www.bizdirusa.com/mirrors/apache/jmeter/binaries/apache-jmeter-2.9.tgz | tar zx",
-		creates => "/vagrant/apache-jmeter-2.9",		
+		creates => "/home/vagrant/apache-jmeter-2.9",		
 		timeout => 0,
-		cwd => "/vagrant",
+		cwd => "/home/vagrant",
 		user => "vagrant",
 		path => "/usr/bin/:/bin",
 		#logoutput => true,		
