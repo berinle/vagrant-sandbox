92a9fb22915d56719ff464d01819aaca2fbef40c
Bayo Erinle <berinle@gmail.com>
2014-01-28 05:49:19 -0500

    corrected jmeter 2.9 download url

--- manifests/main.pp
+++ manifests/main.pp
@@ -11,7 +11,7 @@ package { ["vim",
 
 exec {
 		"set-up-slave-node-1":
-		command => "curl -L http://www.bizdirusa.com/mirrors/apache/jmeter/binaries/apache-jmeter-2.9.tgz | tar zx && mv /home/vagrant/apache-jmeter-2.9 /home/vagrant/jmeter-1",
+		command => "curl -L http://archive.apache.org/dist/jmeter/binaries/apache-jmeter-2.9.tgz | tar zx && mv /home/vagrant/apache-jmeter-2.9 /home/vagrant/jmeter-1",
 		creates => "/home/vagrant/jmeter-1",		
 		timeout => 0,
 		cwd => "/home/vagrant",
@@ -23,7 +23,7 @@ exec {
 
 exec {
 		"set-up-slave-node-2":
-		command => "curl -L http://www.bizdirusa.com/mirrors/apache/jmeter/binaries/apache-jmeter-2.9.tgz | tar zx && mv /home/vagrant/apache-jmeter-2.9 /home/vagrant/jmeter-2",
+		command => "curl -L http://archive.apache.org/dist/jmeter/binaries/apache-jmeter-2.9.tgz | tar zx && mv /home/vagrant/apache-jmeter-2.9 /home/vagrant/jmeter-2",
 		creates => "/home/vagrant/jmeter-2",		
 		timeout => 0,
 		cwd => "/home/vagrant",
@@ -35,7 +35,7 @@ exec {
 
 exec {
 		"set-up-slave-node-3":
-		command => "curl -L http://www.bizdirusa.com/mirrors/apache/jmeter/binaries/apache-jmeter-2.9.tgz | tar zx && mv /home/vagrant/apache-jmeter-2.9 /home/vagrant/jmeter-3",
+		command => "curl -L http://archive.apache.org/dist/jmeter/binaries/apache-jmeter-2.9.tgz | tar zx && mv /home/vagrant/apache-jmeter-2.9 /home/vagrant/jmeter-3",
 		creates => "/home/vagrant/jmeter-3",		
 		timeout => 0,
 		cwd => "/home/vagrant",
